840692475f2931266c057e58515476e857dff2c9
VishnuVelagala <Vishnuvelagala@gmail.com>
2024-10-28 12:47:37 +0530

    First Commit

---
+++ src/test/java/com/example/Vishnu/Gitdemo/GitdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Vishnu.Gitdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
